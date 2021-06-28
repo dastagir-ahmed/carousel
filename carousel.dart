a072e94cdd2f0198d8636b45a3b17b58590f2caa
dbestech <dylan@dbestinfo.com>
2021-06-28 21:45:09 +0800

    First commit

---
+++ carousel.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class DestinationCarousel extends StatefulWidget {
+  const DestinationCarousel({required Key key}) : super(key: key);
+
+  @override
+  _DestinationCarouselState createState() => _DestinationCarouselState();
+}
+
+class _DestinationCarouselState extends State<DestinationCarousel> {
+
+  final CarouselController _controller = CarouselController();
+
+  List _isHovering = [false, false, false, false, false, false, false];
+  List _isSelected = [true, false, false, false, false, false, false];
+
+  int _current = 0;
+
+  final List<String> images = [
+    'assets/images/asia.jpg',
+    'assets/images/africa.jpg',
+    'assets/images/europe.jpg',
+    'assets/images/south_america.jpg',
+    'assets/images/australia.jpg',
+    'assets/images/antarctica.jpg',
+  ];
+
+  final List<String> places = [
+    'ASIA',
+    'AFRICA',
+    'EUROPE',
+    'SOUTH AMERICA',
+    'AUSTRALIA',
+    'ANTARCTICA',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
